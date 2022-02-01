60b3c7a7b1f6663016fffb97aba722e7c055179c
Asad <asad@eteamid.com>
2022-02-01 15:34:28 +0500

    Update gophish.go

--- gophish.go
+++ gophish.go
@@ -58,8 +58,7 @@ var (
 
 func main() {
 	// Load the version
-	port := os.Getenv("PORT")
-	http.ListenAndServe(":"+port, nil)
+
 	version, err := ioutil.ReadFile("./VERSION")
 	if err != nil {
 		log.Fatal(err)
